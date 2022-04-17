4a2d0e6b62c5815fad373f7680df8c6140d6d4b7
David <dave@roguewaverepairs.com>
2022-04-17 11:13:34 -0700

    Initial Commit

---
+++ Project 7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project 7
+//
+//  Created by David Nicklas on 4/10/22.
+//
+
+import Foundation
+
+struct Petition {
+    var title: String
+    var body: String
+    var signitureCount: Int
+}
---
+++ Project 7/Petitions.swift
@@ -0,0 +1,13 @@
+//
+//  Petitions.swift
+//  Project 7
+//
+//  Created by David Nicklas on 4/10/22.
+//
+
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
